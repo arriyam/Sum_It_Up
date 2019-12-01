8e831778378353869975def96fbfa32c63980b67
Maxime <michet.maxime@gmail.com>
2019-12-01 00:58:22 -0500

    Changed procfile

--- src/server.js
+++ src/server.js
@@ -9,6 +9,18 @@ var clientID = []
 var plays= 0;
 
 
+//Playing variables:
+var passageChosed;
+var already = false;
+var submitted = false
+var player1IN;
+var player2IN;
+var results = {
+	player1: undefined,
+	player2: undefined
+}
+
+
 app.use(express.static('../public')); //show static files in 'public' directory
 console.log('Server is running');
 const io = socket(server);
@@ -37,20 +49,52 @@ io.on('connection', (socket) => {
 		}
 
 		socket.on('comfirmCo', (connectionStatus) => {
-			console.log('cofirmco')
 			let connectionStatusComfirm = connectionStatus; //sets value to another variable
 			socket.broadcast.emit('startGame', connectionStatusComfirm); //sends back the cofirmation
 
 		})
 
+		socket.on('chosePassage', () => {
+			if(!already) {
+				passageChosed = getRandomInt(10)
+				already = true;
+			}
+			socket.emit('passageChosen', passageChosed)
+		})
+
+
+		socket.on('answerIN', (answer) => {
+			if (submitted) {
+				player2IN = answer;
+			}
+			if (!submitted) {
+			player1IN = answer;
+			submitted = true;
+			}
+			console.log("1: " + player1IN)
+			console.log("2: " + player2IN)
+			//--------------
+			//call function to judge and give percentage
+			
+			player1OUT = 63;
+			player2OUT = 78;
+			results = {
+				player1: player1OUT,
+				player2: player2OUT
+			
+			}
+			socket.emit('results', results)
+	
+		}) 
+
 	}
 		
 	
 
-
-	// }
-	// socket.on('message', (data) => {
-	// 	//console.log('yes')
-	// 	console.log(data);
-	// })
 })
+
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+  }
+  
