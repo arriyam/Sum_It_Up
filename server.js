562fef74d2807016399a7076706ed263eb26b8a0
Maxime <michet.maxime@gmail.com>
2019-12-01 01:40:20 -0500

    reset

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log('test')
 const express = require('express'); //requires express module
 const socket = require('socket.io'); //requires socket.io module
 const app = express();
@@ -22,7 +21,7 @@ var results = {
 }
 
 
-app.use(express.static('../public')); //show static files in 'public' directory
+app.use(express.static('public')); //show static files in 'public' directory
 console.log('Server is running');
 const io = socket(server);
 
